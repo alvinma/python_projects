290161feba9f8cb69f4b656363838a72cc14209c
alvinma <alvinma111@gmail.com>
2018-10-18 02:29:46 -0700

    Adding comments and check
    Adding extra comments.
    Adding a check for invalid input.

--- For complete beginners/mean_and_sd.py
+++ For complete beginners/mean_and_sd.py
@@ -1,28 +1,35 @@
+"""Simple application to calculate the average of provided values"""
 import pandas as pd
 
 true = 1
 sum = 0
+
+# Open text file to write and store values to find overall average.
 file = open("Number_file.txt","w")
 
 while(true):
-	n = input("Enter the number you want to save in file. leave blank to escape :")
-	if n == '':
-		true = 0
-	else:
-		file.write(n)
-		file.write("\n")
+    n = input("Enter the number you want to save in file. leave blank to escape :")
+    if n == '':
+        true = 0
+    elif not n.isdigit():
+        print ("Please enter a valid input")
+    else:
+        file.write(n)
+        file.write("\n")
 
 file.close()
 
+# Re-Open text file and extract data points from file.
 file = open("Number_file.txt","r")
 element_list = file.readlines()
 file.close()
 
 for element in element_list:                           #calculating mean by sum and divide method
-	sum += element
+    sum += element
 
 mean1 = sum/len(element_list) 
 
+# Create readable list of data points
 l2 = list(map(lambda x : int(x), l2))
 dataframe = pd.DataFrame(l2)
 
